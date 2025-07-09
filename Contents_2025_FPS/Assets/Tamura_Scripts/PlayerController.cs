1ddabb8d57b49c52e9d763ab4fa5dcfe4d01f0c5
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-07-10 08:14:30 +0900

    Playerの挙動追加
    梯子の上り下りを追加しました

--- Contents_2025_FPS/Assets/Tamura_Scripts/PlayerController.cs
+++ Contents_2025_FPS/Assets/Tamura_Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -26,10 +27,11 @@ public class PlayerController : MonoBehaviour
     Vector3 moveDir = Vector3.zero; // 移動方向
     Vector3 moveValue = Vector3.zero; // 移動する量
     float currentSpeed = 0.0f; // 現在のスピードを取得
+    float climingY = 0.0f; // 梯子を上るとき用の変数
     bool isJump = false; // ジャンプ用のフラグ
     bool isCrouch = false; // しゃがみ用のフラグ
     bool isDash = false; // ダッシュ用のフラグ
-
+    bool isCliming = false; // 梯子を上るとき用のフラグ
     // ------------------------------------------関数---------------------------------------
 
 
@@ -102,7 +104,7 @@ public class PlayerController : MonoBehaviour
             }
 
             // スピードとコライダーの設定
-            if (isCrouch)
+            if (isCrouch && !isCliming) // (梯子を上るときはしゃがめないようにする)
             {
                 currentSpeed = crouchMoveSpeed;
                 capsuleCollider.height = crouchHeight;
@@ -124,22 +126,33 @@ public class PlayerController : MonoBehaviour
 
             CrouchCamera();
 
-            // 移動方向を設定
-            float dirX = Input.GetAxisRaw("Horizontal");
-            float dirZ = Input.GetAxisRaw("Vertical");
-            //　カメラの向いている方向に進行方向を合わせる(後ろ、左はマイナスの値として評価する)
-            Vector3 forward = cameraTransform.forward; // カメラの前方向の値を取得
-            Vector3 right = cameraTransform.right; // カメラの右方向の値を取得
-            // y軸方向には関与しない(水平)
-            forward.y = 0;
-            right.y = 0;
+            // 梯子に接触していないなら
+            if (!isCliming)
+            {
+                // 移動方向を設定
+                float dirX = Input.GetAxisRaw("Horizontal");
+                float dirZ = Input.GetAxisRaw("Vertical");
+                //　カメラの向いている方向に進行方向を合わせる(後ろ、左はマイナスの値として評価する)
+                Vector3 forward = cameraTransform.forward; // カメラの前方向の値を取得
+                Vector3 right = cameraTransform.right; // カメラの右方向の値を取得
+                                                       // y軸方向には関与しない(水平)
+                forward.y = 0;
+                right.y = 0;
 
-            // 個別の方向を正規化
-            forward.Normalize();
-            right.Normalize();
+                // 個別の方向を正規化
+                forward.Normalize();
+                right.Normalize();
 
-            // 移動ベクトルの計算
-            moveDir = (dirZ * forward + dirX * right).normalized;
+                // 移動ベクトルの計算
+                moveDir = (dirZ * forward + dirX * right).normalized;
+            }
+            // 梯子を上るとき用
+            else
+            {
+                climingY = Input.GetAxis("Vertical"); // 上下方向
+            }
+
+
         }
     }
 
@@ -148,8 +161,17 @@ public class PlayerController : MonoBehaviour
     {
         if (gameObject != null)
         {
-            moveValue = moveDir * currentSpeed;
-            rb.velocity = new Vector3(moveValue.x, rb.velocity.y, moveValue.z); // 移動量を代入
+            if (!isCliming)
+            {
+                moveValue = moveDir * currentSpeed;
+                rb.velocity = new Vector3(moveValue.x, rb.velocity.y, moveValue.z); // 移動量を代入
+            }
+            else
+            {
+                // rb.useGravity = false; // 落下しないようにする
+                rb.velocity = new Vector3(0, climingY * currentSpeed, 0);
+            }
+
         }
     }
 
@@ -159,15 +181,15 @@ public class PlayerController : MonoBehaviour
         float tagetY = isCrouch ? crouchCameraY : standCameraY; // しゃがみフラグがtrueかfalseか判断
         Vector3 currentPos = cameraTransform.localPosition; // 現在のカメラの位置を取得
         float newY = Mathf.Lerp(currentPos.y, tagetY, Time.deltaTime * crouchSpeed); // targerYまでの値を補完する(crouchSpeedの値で動かす)
-        cameraTransform.localPosition = new Vector3 (currentPos.x, newY, currentPos.z); // カメラの位置を設定
+        cameraTransform.localPosition = new Vector3(currentPos.x, newY, currentPos.z); // カメラの位置を設定
     }
 
 
     //　ジャンプ用の入力
     void InputJump()
     {
-       
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump && !isCliming)
         {
             rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
             isJump = true;
@@ -192,10 +214,61 @@ public class PlayerController : MonoBehaviour
     //　着地用
     void OnCollisionEnter(Collision other)
     {
-        if(!isJump) return; // ジャンプ中でないなら終了
+        if (!isJump) return; // ジャンプ中でないなら終了
         if (other.gameObject.CompareTag("Ground")) // フィールドのタグをGroundにしているがフィールド班の要望によって変更可
         {
             isJump = false;
         }
     }
+
+
+    // 梯子用
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            float vertical = Input.GetAxis("Vertical");
+            if (vertical != 0f && !isCliming)
+            {
+                isCliming = true;
+                rb.useGravity = false;
+                rb.velocity = Vector3.zero;
+                Debug.Log("梯子に入った（登り/降り）");
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("LadderBottom"))
+        {
+            // Sキー（下方向）を押していないなら、Climbing中断しない
+            float vertical = Input.GetAxis("Vertical");
+
+            // 登ってる状態で、入力が上なら Climbing継続、それ以外なら中断
+            if (isCliming && vertical <= 0f)
+            {
+                isCliming = false;
+                rb.useGravity = true;
+                Debug.Log("下端で降りたよ");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("LadderTop"))
+        {
+            float vertical = Input.GetAxis("Vertical");
+            if (isCliming && vertical > 0f)
+            {
+                isCliming = false;
+                rb.useGravity = true;
+                rb.velocity = cameraTransform.forward * moveSpeed;
+                Debug.Log("梯子の上に到達（前進）");
+            }
+        }
+    }
+
 }
