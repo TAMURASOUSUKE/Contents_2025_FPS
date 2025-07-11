9c08dade09f3b1e52ca01476376a96b33cef4f32
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-07-11 22:51:14 +0900

    Cameraの角度修正
    Playerのカメラアングルの下限上限を調整しました

--- Contents_2025_FPS/Assets/Tamura_Scripts/CameraController.cs
+++ Contents_2025_FPS/Assets/Tamura_Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -7,8 +8,9 @@ public class CameraController : MonoBehaviour
     //  ------------------------------------------------------変数------------------------------------------------
     [SerializeField] Transform playerBody; // プレイヤー(親オブジェクト)
     [SerializeField] Transform cameraPivot; // カメラの基点
+    [SerializeField] float topCameraLimit = 0.0f; // カメラの上側向きの制限
+    [SerializeField] float bottomCameraLimit = 0.0f; //　カメラの下側の向きの制限
     [SerializeField] float sensitivity = 2f; // 感度
-    [SerializeField] float cameraCheckDistance = 0.2f; // カメラが壁にめり込むのを防ぐ距離
     float xRotation = 0f; // 上下回転の蓄積
 
 
@@ -32,7 +34,8 @@ public class CameraController : MonoBehaviour
 
         //　上下の回転処理
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -60.0f, 90.0f); // 視点の上限下限を設定
+        // 視点の上限下限を設定
+        xRotation = Mathf.Clamp(xRotation, topCameraLimit, bottomCameraLimit); // X軸回転は上に行くほど負の値を出す
 
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0); // 回転を適用
 
