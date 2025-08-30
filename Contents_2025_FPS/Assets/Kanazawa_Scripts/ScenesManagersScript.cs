82e2b10183591dd93c5b5bbfc88acc8d5342b0b5
Knazawa-Yuusei <yuutaku607@gmail.com>
2025-08-30 18:58:51 +0900

    ゲームオーバーシーンの登録
    ゲームオーバーシーンの登録

--- Contents_2025_FPS/Assets/Kanazawa_Scripts/ScenesManagersScript.cs
+++ Contents_2025_FPS/Assets/Kanazawa_Scripts/ScenesManagersScript.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScenesManagersScripts : MonoBehaviour
 {
     bool isGoal = false;
+    [SerializeField] PlayerController playerController;
+    int currentHP;
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +19,21 @@ public class ScenesManagersScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentHP = playerController.GetHp();
+        if(currentHP <= 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 
-       private void OnTriggerEnter(Collider other)
-       {
-            if (other.CompareTag("Player"))
-            {
-                Debug.Log("Goal");
-                SceneManager.LoadScene("GoalScene");
-            }
-            else
-            {
-            Debug.Log("NULL");
-            }
-       }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           Debug.Log("Goal");
+           SceneManager.LoadScene("GoalScene");
+        }
+
+    }
+ 
 }
