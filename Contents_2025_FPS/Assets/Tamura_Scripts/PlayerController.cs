2a1251e71e33b163f36785bb70fe2ad7faea23c1
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-08-31 17:11:53 +0900

    プレイヤーがゴールする機能、死亡する機能が働いているかのデバッグ
    デバッグ

--- Contents_2025_FPS/Assets/Tamura_Scripts/PlayerController.cs
+++ Contents_2025_FPS/Assets/Tamura_Scripts/PlayerController.cs
@@ -68,7 +68,7 @@ public class PlayerController : MonoBehaviour
     // Hp•ÏX—p(’†g‚ğ•Ï‚¦‚é)
     public void SetHp(int hp)
     {
-        this.hp = this.hp - hp;
+        this.hp = hp;
     }
 
     void LimitHp()
