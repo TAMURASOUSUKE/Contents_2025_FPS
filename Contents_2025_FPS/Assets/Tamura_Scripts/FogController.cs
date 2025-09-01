a33eabbeb07a09242eac95822556fd7912dfdb6e
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-09-01 11:08:00 +0900

    鉄格子とフェンスの判定方法を変更しました
    鉄格子の判定をmeshからboxに変更しました

--- Contents_2025_FPS/Assets/Tamura_Scripts/FogController.cs
+++ Contents_2025_FPS/Assets/Tamura_Scripts/FogController.cs
@@ -13,6 +13,7 @@ public class FogController : MonoBehaviour
     [SerializeField] Transform endPoint; // fogの操作を終えるところ(一番濃い状態)
     [SerializeField] float startDuration = 0.27f; // 最初のfogの強さ
     [SerializeField] float maxDuration = 0.8f; // 霧の最大の濃さ
+    [SerializeField] float adj = 0.5f; // 霧の増加を調整する値
     float startValue = 0f; // 計算上の最初の値
     float endValue = 1f; // 計算上の最後の値
     void Start()
@@ -37,10 +38,10 @@ public class FogController : MonoBehaviour
         float t = Mathf.InverseLerp(zStart, zEnd, zPlayer);
 
         // 値を補完
-        float value = Mathf.Lerp(startValue, endValue, t * 0.5f);
+        float value = Mathf.Lerp(startValue, endValue, t);
 
         // 実際に代入する
-        float totalDuration = value + startDuration;
+        float totalDuration = (value * adj)  + startDuration;
         if (totalDuration > maxDuration)
         {
             totalDuration = maxDuration; // 最大値を設定
