d6e9d50ec439182ed8bd75068d0c1db29abc64b5
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-09-02 13:36:45 +0900

    炎の設計修正
    炎の設計を修正しました

--- Contents_2025_FPS/Assets/Tamura_Scripts/FireController.cs
+++ Contents_2025_FPS/Assets/Tamura_Scripts/FireController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FireController : MonoBehaviour
 {
-   
+
     [SerializeField] Sprite[] sprites = new Sprite[15]; // アニメーションする枚数分用意
     [SerializeField] SpriteRenderer spriteRenderer;
-    [SerializeField] int drawFps;
+    [SerializeField] int drawFps = 2;
+    [SerializeField] float near = 4f;
+    [SerializeField] float far = 8f;
     Camera cam; // メインカメラ
     GameObject player; // プレイヤー
+    Color renderColor;
     int index = 0; // インデックス操作用
     bool isInCamera = false; // カメラ内に入っているか
+    float renderDistance = 10.0f; // 描画し始める距離
     void Start()
     {
         cam = Camera.main; // メインカメラを探す
@@ -24,16 +29,20 @@ public class FireController : MonoBehaviour
     {
         if (isInCamera)
         {
+            RenderFire();
             FireAnim();
         }
+       
     }
 
     private void LateUpdate()
     {
         if (isInCamera)
         {
+            RenderFire();
             LookAnim();
         }
+       
     }
 
 
@@ -59,6 +68,19 @@ public class FireController : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
         }
 
+    }
+
+    void  RenderFire()
+    {
+        Vector3 targetPos = player.transform.position; // プレイヤーの位置
+        Vector3 firePos = gameObject.transform.position; // 炎の位置
+
+        float d2 = (targetPos - firePos).sqrMagnitude; // 距離を2乗した値を出す
+        float a = 1f - Mathf.InverseLerp(near * near, far * far, d2); // 1 -...で値を反転させる
+
+        var c = spriteRenderer.color;
+        c.a = a;
+        spriteRenderer.color = c;
     }
 
     // 写っていない時はフラグを立てない
