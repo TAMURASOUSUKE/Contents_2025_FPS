aa38b401ba646995f7a4f184b5d2409cdbfe7c33
kazu06121 <ymkazu1@gmail.com>
2025-09-07 00:11:56 +0900

    文字の生成の修正
    時間差で文字が出てくるように修正

--- Contents_2025_FPS/Assets/Konishi_Scripts/ImageGenerator.cs
+++ Contents_2025_FPS/Assets/Konishi_Scripts/ImageGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ImageGenerator : MonoBehaviour
 {
     public int imageMax;
+    float imageTimer;
+    float imageCreateTime = 2f;
     public Canvas canvas;
     public GameObject[] image;
     ColorManager colorManager;
@@ -12,18 +14,28 @@ public class ImageGenerator : MonoBehaviour
     void Start()
     {
         colorManager = FindObjectOfType<ColorManager>();
-        
     }
 
     void Update()
     {
+        ImageCreate();
+    }
+
+    void ImageCreate()
+    {
         if (colorManager.isColorChange)
         {
-            int index = Random.Range(0, image.Length);
-            GameObject prefab = image[index];
-            GameObject newImage = Instantiate(prefab, canvas.transform);
-            RectTransform rt = newImage.GetComponent<RectTransform>();
-            rt.anchoredPosition = Vector2.zero;
+            imageTimer += Time.deltaTime;
+            if (imageTimer > imageCreateTime)
+            {
+                int index = Random.Range(0, image.Length);
+                GameObject prefab = image[index];
+                GameObject newImage = Instantiate(prefab, canvas.transform);
+                RectTransform rt = newImage.GetComponent<RectTransform>();
+                rt.anchoredPosition = Vector2.zero;
+                imageTimer = 0;
+            }
         }
     }
+
 }
