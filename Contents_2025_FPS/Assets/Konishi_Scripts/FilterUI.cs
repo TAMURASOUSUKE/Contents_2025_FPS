3e148cfabadf3973631b2ffc5edbfe250bda749d
kazu06121 <ymkazu1@gmail.com>
2025-09-10 21:40:32 +0900

    FilterUIの進行

--- Contents_2025_FPS/Assets/Konishi_Scripts/FilterUI.cs
+++ Contents_2025_FPS/Assets/Konishi_Scripts/FilterUI.cs
@@ -5,7 +5,18 @@ using UnityEngine.UI;
 
 public class FilterUI : MonoBehaviour
 {
+    [SerializeField] ColorManager Color;
     Image image;
+    public float redRatation = 15f;
+    public float greenRatation = 260f;
+    public float blueRatation = 135f;
+    bool isRedColor = false;
+    bool isGreenColor = false;
+    bool isBlueColor = false;
+    bool isWhiteColor = false;
+
+
+
     void Start()
     {
         image = GetComponent<Image>();
@@ -13,7 +24,14 @@ public class FilterUI : MonoBehaviour
 
     void Update()
     {
+        isRedColor = true;
+        isGreenColor = true;
+        isBlueColor = true;
+        isWhiteColor = true;
+
         float speed = 0f;
         image.rectTransform.Rotate(0, 0, speed * Time.deltaTime);
+
     }
 }
+
